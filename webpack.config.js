0a46a121a38578bd9ca25863f20cee3a7ac6b5a5
Luis Sandoval Monestel <79773016+LuisiitoDev@users.noreply.github.com>
2021-06-06 12:44:36 -0600

    it was installed and configured webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+// with this we request the plugin previous installed
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js', //with this we say, what it is the main file and where it is
+    output: { // with this we say, where it will be located our project and the compiled js result
+        path: path.resolve(__dirname, 'dist'), // it indicates the folder name where our compiled javascript will be
+        filename: 'bundle.js' // All our compiled javascript will be created with this name
+    },
+    resolve: { // it indicates what extensions will be listened for this compilation
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            // FIRST RULE, IT WILL BE USED TO IDENTIFY OUR JS AND JSX FILES AND THE LOADER WHICH WILL BE USED (BABEL)
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    // WE CONFIGURE OUR PLUGINS TO USE
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+    ],
+    // WE CONFIGURE OUR SERVER WHICH WILL RUN ON DEV ENVIROMENT
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3005
+    }
+};
